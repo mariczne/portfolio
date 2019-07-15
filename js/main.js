63dd698e97f77eb1711c2472de45a29e0716b7c6
Marcin <mariczne@gmail.com>
2019-07-15 14:28:23 +0200

    Adding project component, reorganising sass code

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ function navigationUnderlineHandler(entries, observer) {
 }
 
 navigationUnderlineObserver.observe(heroLinesBlue);
+
+// Sections fade in/out
+const sectionObserver = new IntersectionObserver(sectionsHandler, 
+  { root: null, rootMargin: '-4.5% 0% 0% 0%', threshold: 0.1 }
+);
+
+function sectionsHandler(entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry.target.className, entry.intersectionRatio);
+    entry.target.style.opacity = (entry.intersectionRatio > 0.1) ? 100 : 0;
+  })
+}
+
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section => {
+  sectionObserver.observe(section)
+})
