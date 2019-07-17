1048cd50a3fa313472076b29da217a6744ba3777
Marcin <mariczne@gmail.com>
2019-07-17 18:55:47 +0200

    Adding contact form, footer

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 // Navigation underline
-const heroLinesBlue = document.querySelector('.hero__lines--blue');
-const navigationUnderline = document.querySelector('.navigation__underline');
+const heroLineBlue = document.querySelector('.hero__line--blue');
+const navigationUnderlines = document.querySelector('.navigation__underlines');
 
-const navigationUnderlineObserver = new IntersectionObserver(navigationUnderlineHandler,
+const navigationUnderlinesObserver = new IntersectionObserver(navigationUnderlinesHandler,
   { root: null, rootMargin: '-4.5% 0% 0% 0%', threshold: [0, 1] }
 );
 
-function navigationUnderlineHandler(entries, observer) {
+function navigationUnderlinesHandler(entries, observer) {
   entries.forEach(entry => {
-    navigationUnderline.style.display = entry.isIntersecting ? 'none' : 'block';
+    navigationUnderlines.style.display = entry.isIntersecting ? 'none' : 'block';
   })
 }
 
-navigationUnderlineObserver.observe(heroLinesBlue);
+navigationUnderlinesObserver.observe(heroLineBlue);
 
 // Sections fade in/out
 const sectionObserver = new IntersectionObserver(sectionsHandler, 
-  { root: null, rootMargin: '-4.5% 0% 0% 0%', threshold: 0.1 }
+  { root: null, rootMargin: '-4.5% 0% 0% 0%', threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] }
 );
 
 function sectionsHandler(entries, observer) {
   entries.forEach(entry => {
-    console.log(entry.target.className, entry.intersectionRatio);
-    entry.target.style.opacity = (entry.intersectionRatio > 0.1) ? 100 : 0;
+    // if a section takes 50% or more of the viewport, fade it in
+    entry.target.style.opacity = (entry.intersectionRect.height / entry.rootBounds.height >= 0.5) ? 100 : 0;
   })
 }
 
