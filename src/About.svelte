7a56780cf80b3765c2eaae72287970e8582018b5
mariczne <mariczne@gmail.com>
2020-12-23 23:31:41 +0100

    Initial svelte setup

---
+++ src/About.svelte
@@ -0,0 +1,42 @@
+<section class="about" id="about">
+  <div class="about__content">
+    <h2 class="about__header--primary">About me</h2>
+
+    <p>
+      Hi, my name is
+      <strong>Marcin</strong>. I primarily build
+      <strong>user interfaces</strong>, but I also work with server-side
+      technologies.<br />
+      My journey with web development began in mid-2018 and I've been
+      learning something new every day since. I&nbsp;enjoy solving problems
+      and seeing the effect of my work gives me a lot of satisfaction.
+    </p>
+
+    <h3 class="about__header--secondary">
+      Technologies that I have experience with:
+    </h3>
+    <h4>On the front-end, I use:</h4>
+    <ul class="about__list">
+      <li><i class="about__icon icon-html5" />HTML</li>
+      <li><i class="about__icon icon-css3" />CSS (SASS)</li>
+      <li><i class="about__icon icon-javascript" />JavaScript</li>
+      <li><i class="about__icon icon-react" />React</li>
+    </ul>
+
+    <h4>On the back-end, I work with:</h4>
+    <ul class="about__list">
+      <li>
+        <i class="about__icon icon-node-dot-js" />Node.js with Express
+      </li>
+      <li><i class="about__icon icon-database" />SQL or MongoDB</li>
+    </ul>
+
+    <h4>I also use:</h4>
+    <ul class="about__list">
+      <li><i class="about__icon icon-git" />git</li>
+      <li><i class="about__icon icon-linux" />Linux</li>
+    </ul>
+
+    <p>I speak <strong>Polish</strong> and <strong>English</strong>.</p>
+  </div>
+</section>
