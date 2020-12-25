32022caad8e64bc5a2e964097812d29cfcb85557
Marcin <mariczne@gmail.com>
2020-12-25 14:14:58 +0100

    Customize build

--- rollup.config.js
+++ rollup.config.js
@@ -5,87 +5,137 @@ import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import css from "rollup-plugin-css-only";
-import svelteStaticHtml from "./static";
 import smartAsset from "rollup-plugin-smart-asset";
+import vm from "vm";
+import fs from "fs-extra";
+import { rollup } from "rollup";
+import posthtml from "posthtml";
+import beautify from "posthtml-beautify";
+import insertAt from "posthtml-insert-at";
+import typescript from "@rollup/plugin-typescript";
+import image from "svelte-image";
 
 const production = !process.env.ROLLUP_WATCH;
 
-// function serve() {
-//   let server;
+function serve() {
+  let server;
 
-//   function toExit() {
-//     if (server) server.kill(0);
-//   }
+  function toExit() {
+    if (server) server.kill(0);
+  }
 
-//   return {
-//     writeBundle() {
-//       if (server) return;
-//       server = require("child_process").spawn(
-//         "npm",
-//         ["run", "start", "--", "--dev"],
-//         {
-//           stdio: ["ignore", "inherit", "inherit"],
-//           shell: true,
-//         }
-//       );
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require("child_process").spawn(
+        "npm",
+        ["run", "start", "--", "--dev"],
+        {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        }
+      );
 
-//       process.on("SIGTERM", toExit);
-//       process.on("exit", toExit);
-//     },
-//   };
-// }
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    },
+  };
+}
 
 export default {
-  input: "src/index.js",
+  input: "src/main.ts",
   output: {
     format: "iife",
-    // name: "app",
+    name: "main",
     file: "build/bundle.js",
   },
   plugins: [
-    // svelte({
-    //   preprocess: sveltePreprocess(),
-    //   compilerOptions: {
-    //     dev: !production,
-    //   },
-    // }),
-
     // smartAsset({ url: "copy" }),
 
-    // css({ output: "bundle.css" }),
-
-    // resolve({
-    //   browser: true,
-    //   dedupe: ["svelte"],
-    // }),
-
-    // commonjs(),
-
-    svelteStaticHtml({
-      component: "src/App.svelte",
-      output: "build",
-      template: "src/template.html",
-      // preprocess: [sveltePreprocess()],
-      // plugins: [css({ output: "bundle.css" })],
-      // preprocess: sveltePreprocess(),
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
     }),
 
-    // plugins: [
-    //   // smartAsset(),
-    //   // resolve({
-    //   //   browser: true,
-    //   //   dedupe: ["svelte"],
-    //   // }),
-    //   // commonjs(),
-    // ],
+    typescript({ sourceMap: false }),
 
-    // !production && serve(),
-    // !production && livereload("build"),
+    commonjs(),
 
-    // production &&
-    // production && terser(),
+    svelteStaticHtml({
+      component: "src/Index.svelte",
+      output: "build",
+      template: "src/template.html",
+    }),
+
+    !production && serve(),
+    !production && livereload(),
+
+    production && terser(),
   ],
   watch: {
     clearScreen: false,
   },
 };
+
+function svelteStaticHtml({ component, output, template }) {
+  return {
+    name: "svelte-static-html",
+    async writeBundle() {
+      const { generate } = await rollup({
+        input: component,
+        plugins: [
+          resolve({
+            browser: true,
+            dedupe: ["svelte"],
+          }),
+          svelte({
+            preprocess: [sveltePreprocess(), image()],
+            compilerOptions: {
+              dev: !production,
+              generate: "ssr",
+            },
+          }),
+          css(),
+          // !production && serve(),
+          // !production && livereload("src"),
+          production && terser(),
+        ],
+      });
+
+      const bundle = await generate({ format: "cjs" });
+      const entryChunk = bundle.output.find(
+        (chunkOrAsset) => chunkOrAsset.isEntry
+      );
+      const Component = vm.runInNewContext(entryChunk.code, { module });
+      const { html } = Component.render();
+      const htmlTemplate = await generateHtmlTemplate(template, html);
+      const processedHtml = await posthtml(
+        [
+          template && insertAt({ selector: "body", prepend: html }),
+          beautify({
+            rules: {
+              blankLines: false,
+            },
+          }),
+        ].filter(Boolean)
+      ).process(htmlTemplate);
+
+      await fs.outputFile(`${output}/index.html`, processedHtml.html);
+
+      const styles = bundle.output.find(
+        (chunkOrAsset) => chunkOrAsset.fileName === "bundle.css"
+      );
+      await fs.outputFile(`${output}/bundle.css`, styles.source);
+
+      bundle.output.forEach((chunkOrAsset) =>
+        console.log(chunkOrAsset.fileName)
+      );
+    },
+  };
+}
+
+async function generateHtmlTemplate(template) {
+  if (template) {
+    return fs.readFile(template, "utf8");
+  }
+}
