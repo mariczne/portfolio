251374685b6886533336992af4ffa9abe9c87179
Marcin <mariczne@gmail.com>
2020-12-26 12:53:48 +0100

    Add html minifier, mail observer

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,53 @@
 export default (function () {
-  function onload() {
+  function installObservers() {
+    const everyTenPercent = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+    const rootMargin = "-4.5% 0% 0% 0%";
+
     // Navigation underline
-    const heroLineBlue = document.querySelector(".heroline:last-of-type");
-    const navigationUnderlines = <HTMLElement>(
-      document.querySelector(".underlines")
+    const heroLineBlue = document.querySelector<HTMLDivElement>(
+      ".heroline:last-of-type"
+    );
+    const navigationUnderlines = document.querySelector<HTMLDivElement>(
+      ".underlines"
     );
 
     const navigationUnderlinesObserver = new IntersectionObserver(
-      navigationUnderlinesHandler,
-      { root: null, rootMargin: "-4.5% 0% 0% 0%", threshold: [0, 1] }
+      (entries) => {
+        entries.forEach((entry) => {
+          navigationUnderlines.style.display = entry.isIntersecting
+            ? "none"
+            : "block";
+        });
+      },
+      { root: null, rootMargin, threshold: [0, 1] }
     );
-
-    function navigationUnderlinesHandler(entries) {
-      entries.forEach((entry) => {
-        navigationUnderlines.style.display = entry.isIntersecting
-          ? "none"
-          : "block";
-      });
-    }
 
     navigationUnderlinesObserver.observe(heroLineBlue);
 
     // Sections fade in/out
-    const sectionObserver = new IntersectionObserver(sectionsHandler, {
-      root: null,
-      rootMargin: "-4.5% 0% 0% 0%",
-      threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-    });
+    const sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          // if a section takes 20% or more of the viewport, fade it in
+          // because of threshold every 10% intersection ratio
+          // it will not always be perfectly 20%
+          // (also intersection ratio is different to what I'm targeting:
+          // it's % of element visible, not element taking % of viewport)
+          const target = entry.target as HTMLDivElement;
+          target.style.opacity =
+            entry.intersectionRect.height / entry.rootBounds.height >= 0.2
+              ? "100"
+              : "0";
+        });
+      },
+      {
+        root: null,
+        rootMargin,
+        threshold: everyTenPercent,
+      }
+    );
 
-    function sectionsHandler(entries) {
-      entries.forEach((entry) => {
-        // if a section takes 20% or more of the viewport, fade it in
-        // because of threshold every 10% intersection ratio
-        // it will not always be perfectly 20%
-        // (also intersection ratio is different to what I'm targeting:
-        // it's % of element visible, not element taking % of viewport)
-        entry.target.style.opacity =
-          entry.intersectionRect.height / entry.rootBounds.height >= 0.2
-            ? 100
-            : 0;
-      });
-    }
-
-    const sections = document.querySelectorAll("section");
+    const sections = document.querySelectorAll<HTMLDivElement>("section");
 
     sections.forEach((section) => {
       sectionObserver.observe(section);
@@ -55,10 +60,24 @@ export default (function () {
 
     const mail = <HTMLAnchorElement>document.getElementById("mail-link");
 
-    mail.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.location.href = deobfusc(mail.href);
-    });
+    const mailObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const target = entry.target as HTMLAnchorElement;
+          if (entry.isIntersecting) target.href = deobfusc(mail.href);
+          mailObserver.unobserve(target);
+        });
+      },
+      { root: null, rootMargin, threshold: [0, 1] }
+    );
+
+    mailObserver.observe(mail);
+  }
+
+  function onload() {
+    if (window.IntersectionObserver) {
+      installObservers();
+    }
   }
 
   if (typeof window !== "undefined") {
